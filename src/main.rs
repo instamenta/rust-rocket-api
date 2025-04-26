0db4d274aa5ece26ad576b634b7e133c9ded1843
Zhan Milenkov <instamenta@abv.bg>
2025-04-26 15:27:20 +0300

    extending main functionality and getting rocket api setup
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+#[macro_use] extern crate rocket;
+
 use bookstore::db;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
     let pool = db::pool::create_database_pool();
 
     let user_list = db::repositories::user::load_users(pool);
 
     println!("Loaded Users {:?}", user_list);
-    
-    Ok(())
-}
 
+    rocket::build().mount("/", routes![index])
+}
